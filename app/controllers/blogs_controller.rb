c4756b5d77b8874e9bb55beb0dd94249ea5ad8cd
mi-1109 <meisuzuki.sophia@gmail.com>
2021-02-22 05:51:30 +0000

    [Finish]3-3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,13 +1,25 @@
 class BlogsController < ApplicationController
-  def index
+  def index #記事一覧+Topページ
   end
 
-  def show
+  def show #詳細表示ページ
   end
 
   def new
+    @blog = Blog.new
   end
 
-  def edit
+  def create #記事を保存
+    blog = Blog.new(blog_params)
+    blog.save
+    redirect_to blogs_path
+  end
+
+  def edit #記事編集ページ
+  end
+
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :category, :body)
   end
 end
